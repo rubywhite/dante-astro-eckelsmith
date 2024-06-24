11eabb1f588d83fcf981dd45f5f1f82a2395ad7b
John Loughlin <john@rubywhite.com>
2024-06-23 22:19:53 -0700

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,12 +38,12 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
-    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+    title: 'Ruby White',
+    subtitle: 'helping people communicate with technology since 1996',
+    description: 'communication & technology services for non-profits and progressive groups',
     image: {
         src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
+        alt: 'inspiration'
     },
     headerNavLinks: [
         {
@@ -77,27 +77,27 @@ const siteConfig: SiteConfig = {
             href: '/terms'
         },
         {
-            text: 'Download theme',
+            text: 'Download website theme',
             href: 'https://github.com/JustGoodUI/dante-astro-theme'
         }
     ],
     socialLinks: [
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
+            text: 'Mastodon',
+            href: 'https://mastodon.rubywhite.com/@John'
         },
         {
-            text: 'Instagram',
-            href: 'https://instagram.com/'
+            text: 'Github',
+            href: 'https://github.com/rubywhite'
         },
         {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
+            text: 'LinkedIn',
+            href: 'https://www.linkedin.com/in/johnloughlin/'
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Cheers',
+        text: "I'm **John Loughlin**, a technology prognosticator.",
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
@@ -109,11 +109,7 @@ const siteConfig: SiteConfig = {
             }
         ]
     },
-    subscribe: {
-        title: 'Subscribe to Dante Newsletter',
-        text: 'One update per week. All the latest posts directly in your inbox.',
-        formUrl: '#'
-    },
+
     postsPerPage: 8,
     projectsPerPage: 8
 };
